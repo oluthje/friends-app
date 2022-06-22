f3b3082210b9a4e14a4c9d4b08c793c3dd47ba22
Oscar Luthje <oluthje@gmail.com>
2022-06-21 18:27:47 -0700

    Dashboard cards clickable

--- lib/widgets/cards/dashboard_card.dart
+++ lib/widgets/cards/dashboard_card.dart
@@ -19,29 +19,22 @@ class DashboardCard extends StatelessWidget {
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10.0),
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          const Padding(
-            padding: EdgeInsets.only(left: 16.0, top: 16.0),
-            child: Text(
-              'Friends',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-          ),
-          child,
-          TextButton(
-            onPressed: () => onPressed(),
-            child: const Padding(
-              padding: EdgeInsets.only(right: 8.0),
-              child: Align(
-                alignment: Alignment.centerRight,
-                child: Text('View all'),
+      child: InkWell(
+        onTap: () => onPressed(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(left: 16.0, top: 16.0),
+              child: Text(
+                'Friends',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
             ),
-          ),
-        ],
-      ),
+            child,
+          ],
+        ),
+      )
     );
   }
 }
